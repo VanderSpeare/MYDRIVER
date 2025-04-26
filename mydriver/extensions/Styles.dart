d949d3bfeebd07eb23a9ae96bd3bdee3d85f2944
Phạm Đại Trí <125333024+VanderSpeare@users.noreply.github.com>
2025-04-26 13:15:11 +0700

    Add files via upload

---
+++ mydriver/extensions/Styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'Colors.dart';
+
+TextStyle HeaderStyle() {
+  return TextStyle(
+    color:jcbDarkColor,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    fontSize: 26.0,
+    height: 0.5,
+  );
+}
+
+TextStyle bodyStyle() {
+  return TextStyle(color: jcbDarkColor, fontSize: 14.0);
+}
